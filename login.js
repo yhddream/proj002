410a0d16138a52e74f0106c0cd0961693b01245d
yhddream <yhdream@naver.com>
2021-09-24 22:39:22 +0900

    login

---
+++ login.js
@@ -0,0 +1,28 @@
+let inputTag = document.getElementsByTagName("input");
+let buttonArray = document.getElementsByTagName("button");
+
+let inputId = inputTag[0];
+let inputPassword = inputTag[1];
+
+let ordinaryButton = buttonArray[0];
+let facebookLoginButton = buttonArray[1];
+
+inputPassword.addEventListener("keyup", () => {
+    if(inputId.value){
+        ordinaryButton.classList.remove("unactivatedLoginColor");
+        ordinaryButton.classList.add("activatedLoginColor");
+    }
+    if(!inputPassword.value){
+        ordinaryButton.classList.remove("activatedLoginColor");
+        ordinaryButton.classList.add("unactivatedLoginColor");
+    }
+})
+
+ordinaryButton.addEventListener('click', () => {
+    if(inputId.value === "aaa" && inputPassword.value === "111"){
+        alert("로그인 성공");
+        window.location = "./index1.html";
+    } else {
+        alert("로그인 실패");
+    }
+})
